157f1c36f758f52f2b502136f5fe2733a267cb51
Aleksandr Sekker <aleksandr.sekker@gmail.com>
2020-05-12 13:40:19 +0300

    Updates

---
+++ precache-manifest.ee2e2ec94822c9811731af859be14c60.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9bb8f42e9599b6e5225868a64ac208f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e319eff329af44251f6",
+    "url": "/static/css/main.255065ed.chunk.css"
+  },
+  {
+    "revision": "5043021907ec83c4e307",
+    "url": "/static/js/2.83249192.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.83249192.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e319eff329af44251f6",
+    "url": "/static/js/main.c256a0a4.chunk.js"
+  },
+  {
+    "revision": "e4effbdd1a3ce8bff063",
+    "url": "/static/js/runtime-main.cbcf8a8e.js"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "ba5cde21eeea0d57ab7efefc99596cce",
+    "url": "/static/media/OpenSans-SemiBold.ba5cde21.ttf"
+  },
+  {
+    "revision": "fd5c21569c41999ec688c845ed47cd20",
+    "url": "/static/media/animation1.fd5c2156.png"
+  },
+  {
+    "revision": "4dc85deafc48f2be628e9fe0d2c4fa57",
+    "url": "/static/media/animation2.4dc85dea.png"
+  },
+  {
+    "revision": "7466eac61dd756ef20e7853fa34ac225",
+    "url": "/static/media/blog.7466eac6.svg"
+  },
+  {
+    "revision": "ecf3f4af9526df484e8a3c1bffd0da34",
+    "url": "/static/media/facebook.ecf3f4af.svg"
+  },
+  {
+    "revision": "f91d1ca85a85cdea2e5053e5ee01cb95",
+    "url": "/static/media/fone2.f91d1ca8.png"
+  },
+  {
+    "revision": "a4c28ae0f911bdb7cafbc240d1d5e8a5",
+    "url": "/static/media/fonedown.a4c28ae0.png"
+  },
+  {
+    "revision": "da4c7e80e9f57d02249d0f3b4823e4a5",
+    "url": "/static/media/instagram.da4c7e80.svg"
+  },
+  {
+    "revision": "041d0c37be75b910fb966cf5a65319a6",
+    "url": "/static/media/telegram.041d0c37.svg"
+  },
+  {
+    "revision": "f69a031ce0e99e6a4972db258278040b",
+    "url": "/static/media/vk.f69a031c.svg"
+  },
+  {
+    "revision": "a0133af11058fbd817ad3a7eee579176",
+    "url": "/static/media/youtube.a0133af1.svg"
+  },
+  {
+    "revision": "b41ac76965733d075d6fa3cb5c9a942e",
+    "url": "/static/media/лого.b41ac769.svg"
+  },
+  {
+    "revision": "af0fa252fc93e5a137c3c262c84c6f01",
+    "url": "/static/media/розеткаМонтажная область 1 копия 10.af0fa252.svg"
+  },
+  {
+    "revision": "b027831bad82fe526b9073c534175d26",
+    "url": "/static/media/розеткаМонтажная область 1 копия 11.b027831b.svg"
+  },
+  {
+    "revision": "9cefebf9c3212fd88d4e20ebe41c3911",
+    "url": "/static/media/розеткаМонтажная область 1 копия 2.9cefebf9.svg"
+  },
+  {
+    "revision": "8f300915d5810bb15ee53559ea9646cd",
+    "url": "/static/media/розеткаМонтажная область 1 копия 3.8f300915.svg"
+  },
+  {
+    "revision": "008e8d7774aa5822942b4bd5775354f8",
+    "url": "/static/media/розеткаМонтажная область 1 копия 4.008e8d77.svg"
+  },
+  {
+    "revision": "dd9240f532f85d45134796fb02db69f7",
+    "url": "/static/media/розеткаМонтажная область 1 копия 5.dd9240f5.svg"
+  },
+  {
+    "revision": "3d2df432ab5481a16ae5d87c1615b41e",
+    "url": "/static/media/розеткаМонтажная область 1 копия 6.3d2df432.svg"
+  },
+  {
+    "revision": "dfc01cd9e235730147c9dc05df10bf4d",
+    "url": "/static/media/розеткаМонтажная область 1 копия 7.dfc01cd9.svg"
+  },
+  {
+    "revision": "d25066933e8b0093aa0075e9ef21ed14",
+    "url": "/static/media/розеткаМонтажная область 1 копия 8.d2506693.svg"
+  },
+  {
+    "revision": "134e7349466192b29bf95ca519505dfd",
+    "url": "/static/media/розеткаМонтажная область 1 копия 9.134e7349.svg"
+  },
+  {
+    "revision": "511d3d626f43fa2c794805714a10054c",
+    "url": "/static/media/розеткаМонтажная область 1 копия.511d3d62.svg"
+  },
+  {
+    "revision": "e372debfa1edd8f7a89192dcb84c4e73",
+    "url": "/static/media/розеткаМонтажная область 1.e372debf.svg"
+  }
+]);
